4be0a962f6270084b44750a2881631edb1c61fa8
Vlad Rublevskiy <vladislav.rublevskiy@gmail.com>
2019-11-14 12:50:52 +0300

    Add CommandHandler interface

---
+++ src/main/java/by/bsuir/exchange/chain/CommandHandler.java
@@ -0,0 +1,18 @@
+package by.bsuir.exchange.chain;
+
+import by.bsuir.exchange.command.CommandEnum;
+import by.bsuir.exchange.manager.exception.ManagerInitializationException;
+import by.bsuir.exchange.manager.exception.ManagerOperationException;
+
+import javax.servlet.http.HttpServletRequest;
+
+@FunctionalInterface
+public interface CommandHandler {
+
+    boolean handle(HttpServletRequest request, CommandEnum command) throws ManagerInitializationException,
+                                                                            ManagerOperationException;
+
+    default CommandHandler chain(CommandHandler other){
+        return (request, command) -> CommandHandler.this.handle(request, command) && other.handle(request, command);
+    }
+}
