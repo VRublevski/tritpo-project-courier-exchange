1b48bfe70468e6be29ab4546b4737f37aa805a29
Vlad Rublevskiy <vladislav.rublevskiy@gmail.com>
2019-11-17 22:41:44 +0300

    Refactor PageAttributesProvider

--- src/main/java/by/bsuir/exchange/provider/PageAttributesNameProvider.java
+++ src/main/java/by/bsuir/exchange/provider/PageAttributesNameProvider.java
@@ -1,45 +1,15 @@
 package by.bsuir.exchange.provider;
 
 
-import java.util.HashMap;
-import java.util.Map;
-
-
 public class PageAttributesNameProvider {
-    private static final Map<String, String> globalPage = new HashMap<>();
-    private static final Map<String, String> loginPage = new HashMap<>();
-    private static final Map<String, String> registerPage = new HashMap<>();
 
     public static final String GLOBAL_PAGE = "GLOBAL_PAGE";
     public static final String LANG = "LANG";
 
     public static final String LOGIN_PAGE = "LOGIN_PAGE";
     public static final String REGISTER_PAGE = "REGISTER_PAGE";
-    public static final String USER_ATTRIBUTE = "USER_ATTRIBUTE";
+    public static final String USER_ATTRIBUTE = "user";
+    public static final String CLIENT_ATTRIBUTE = "client";
+    public static final String COURIER_ATTRIBUTE = "courier";
 
-
-    static {
-        globalPage.put(LANG, "lang");
-        globalPage.put(USER_ATTRIBUTE, "user");
-    }
-
-    public static String getProperty(String page, String attribute){
-        String result = null;
-        if (!page.equals(GLOBAL_PAGE)){
-            switch (page){
-                case LOGIN_PAGE : {
-                    result = loginPage.get(attribute);
-                    break;
-                }
-                case REGISTER_PAGE : {
-                    result = registerPage.get(attribute);
-                    break;
-                }
-            }
-        }
-        if (result == null){
-            result = globalPage.get(attribute);
-        }
-        return result;
-    }
 }
