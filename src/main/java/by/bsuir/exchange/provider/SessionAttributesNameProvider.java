0874d2c7e515fb0b76454ab80cb3db5dbc655ad5
Vlad Rublevskiy <vladislav.rublevskiy@gmail.com>
2019-11-18 23:27:51 +0300

    Refactor SessionAtributesNameProvider

--- src/main/java/by/bsuir/exchange/provider/SessionAttributesNameProvider.java
+++ src/main/java/by/bsuir/exchange/provider/SessionAttributesNameProvider.java
@@ -1,24 +1,10 @@
 package by.bsuir.exchange.provider;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class SessionAttributesNameProvider {
-    private static final Map<String, String> attributes = new HashMap<>();
+    public static final String ROLE = "role";
+    public static final String ID = "id";
+    public static final String LANG = "lang";
+    public static final String RU = "ru_RU";
+    public static final String EN = "en_EN";
 
-    public static final String ROLE = "ROLE";
-    public static final String LANG = "LANG";
-    public static final String RU = "RU";
-    public static final String EN = "EN";
-
-    static {
-        attributes.put(ROLE, "role");
-        attributes.put(LANG, "lang");
-        attributes.put(RU, "ru_RU");
-        attributes.put(EN, "en_EN");
-    }
-
-    public static String getProperty(String name){
-        return attributes.get(name);
-    }
 }
